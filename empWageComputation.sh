830fc05b5f2a95d404e5fb761ea916038a747af4
root <root@MSI.localdomain>
2021-06-16 18:20:44 +0530

    To check employee is present or absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome to empcomputation"
+empcheck=$(( RANDOM % 2 ))
+
+# selection
+if [ $empcheck -eq $IS_PRESENT ]
+then
+      echo "employee is present"
+else
+      echo "employee is absent"
+fi
