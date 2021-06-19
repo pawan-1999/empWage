ae2aa4e8178b56c3e2e4374b62153cd0e8065d36
pawan_1999 <guptapawan767@gmail.com>
2021-06-19 20:08:11 +0530

    wages calculated by case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,23 @@
-#!/bin/bash -x
+#! /bin/bash/ -x
 
-echo "Welcome to empcomputation"
+# constant
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_HALF_TIME=2
+
+EMP_WAGE_PER_HR=20
+
+# variable
+empCheck=$(( RANDOM % 3 ))
+
+case $empCheck in
+        $IS_PRESENT_FULL_TIME)
+                empHrs=8 ;;
+        $IS_PRESENT_HALF_TIME)
+                empHrs=4 ;;
+        *)
+                empHrs=0 ;;
+esac
+
+# calculation
+salary=$(( empHrs * EMP_WAGE_PER_HR ))
+
