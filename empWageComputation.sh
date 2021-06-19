1b43b6e048c2faee66e6f021431a86bbbdc6794e
pawan_1999 <guptapawan767@gmail.com>
2021-06-19 20:16:00 +0530

    Wage Computation for Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,28 @@
-#!/bin/bash -x
+#! /bin/bash -x
 
-echo "Welcome to empcomputation"
+#constant
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_HALF_TIME=2
+EMP_WAGE_PER_HR=20
+MAX_WORKING_DAY=20
+
+
+for  ((day=1;day<=$MAX_WORKING_DAY;day++))
+do
+        #variable
+        empCheck=$(( RANDOM % 3 ))
+
+        #selection
+        case $empCheck in
+                $IS_PRESENT_FULL_TIME)
+                        empHrs=8 ;;
+                $IS_PRESENT_HALF_TIME)
+                        empHrs=4 ;;
+                *)      empHrs=0 ;;
+        esac
+        #calculation
+        totalWorkingHours=$(( totalWorkingHours + empHrs ))
+        salary=$(( empHrs * EMP_WAGE_PER_HR ))
+done
+totalSalary=$(( totalWorkingHours * EMP_WAGE_PER_HR ))
+
