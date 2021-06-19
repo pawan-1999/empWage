76d4245a45a3d5f7f290b9f740ff8f2aed2f4ba5
pawan_1999 <guptapawan767@gmail.com>
2021-06-19 19:59:24 +0530

    Full time and Part time wages Calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,25 @@
-#!/bin/bash -x
+#! /bin/bash/ -x
 
-echo "Welcome to empcomputation"
+# constant
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_HALF_TIME=2
+
+EMP_WAGE_PER_HR=20
+
+# variable
+empCheck=$(( RANDOM % 3 ))
+
+# selection
+if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
+then
+        empHrs=8
+elif [ $empCheck -eq $IS_PRESENT_HALF_TIME ]
+then
+        empHrs=4
+else
+        empHrs=0
+fi
+
+# calculation
+salary=$(( empHrs * EMP_WAGE_PER_HR ))
+
