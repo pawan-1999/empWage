fd10e62c3644d6f26c1e2b15c50065a444abb629
pawan_1999 <guptapawan767@gmail.com>
2021-06-19 19:45:27 +0530

    Wages Calculation of Employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,19 @@
-#!/bin/bash -x
+#! /bin/bash/ -x
 
-echo "Welcome to empcomputation"
+# constant
+IS_PRESENT=1
+EMP_WAGE_PER_HR=20
+
+# variable
+empCheck=$(( RANDOM % 2 ))
+
+# selection
+if [ $empCheck -eq $IS_PRESENT ]
+then
+        empHrs=8
+else
+        empHrs=0
+fi
+
+# calculation
+salary=$(( empHrs * EMP_WAGE_PER_HR ))
